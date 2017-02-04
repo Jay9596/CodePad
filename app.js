0b282091a1c415da7234aad990d7d9f219cbad96
Jay Shah <Jay Shah>
2017-02-04 22:30:06 +0530

    changes in complie( )

--- app.js
+++ app.js
@@ -5,19 +5,12 @@ function compile() {
     js = document.getElementById("js");
     //console.log(html+css+js);
     var code = document.getElementById("output");
-    const Prehtml = "<!DOCTYPE html>\n<html>\n<head>\n<title>Try</title>\n</head><body>\n";
+    const Prehtml = "<!DOCTYPE html>\n<html>\n<head>\n<title>Try</title>\n<style>";
+    const PostStyle = "\n</style>\n</head>\n<body>";
     const Posthtml = "\n</body>\n</html>";
-    code.srcdoc = Prehtml + html.textContent + "<style>" + css.textContent + "</style>" + "<script>" + js.textContent + "</script>" + Posthtml;
+    code.srcdoc = Prehtml + css.textContent + PostStyle + html.textContent + "\n<script>" + js.textContent + "</script>" + Posthtml;
 }  
 
 document.addEventListener("keyup",function(e){
     compile();
 });
-
-function clearAll()
-{
-    html = document.getElementById("html");
-    css = document.getElementById("css");
-    js = document.getElementById("js");
-    html.textContent = css.textContent = js.textContent = "";
-}
