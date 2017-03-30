e1ba3fbf08666408e9e72ed65c14e90b25138a29
Jay Shah <jayshah.9596@gmail.com>
2017-03-30 14:14:14 +0530

    Some more tests  - Chai dependancy added  - Simple tests for basic elements

--- test/test.js
+++ test/test.js
@@ -1,34 +1,111 @@
 const assert = require('assert')
 const Application = require('spectron').Application
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 
+var app = new Application({
+      path: require('electron'),
+      args: [__dirname + '/../app/index.html']
+    })
+global.before(() => {
+  chai.should()
+  chai.use(chaiAsPromised)
+})
 describe('CodePad Testing:', function () {
   this.timeout(12000)
 
   beforeEach(() => {
-    this.app = new Application({
-      path: require('electron'),
-      args: [__dirname + '/../app/index.html']
-    })
-    return this.app.start()
+    return app.start()
   })
+
   afterEach(() => {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop()
+    if (app && app.isRunning()) {
+      return app.stop()
     }
   })
+/*
   it('Window Tests:', () => {
-    this.app.start().then(() => {
-      return this.app.browserWindow.isVisible()
+    app.start().then(() => {
+      return app.browserWindow.isVisible()
     }).then(isVisible => {
       assert.equal(isVisible, true)
     }).then(() => {
-      return this.app.client.getTitle()
+      return app.client.getTitle()
     }).then(title => {
       assert.equal(title, 'CodePad')
-    }).then(() => {
-      return this.app.stop()
     }).catch(err => {
       console.log('Test Failed:', err.message)
+    })
+  })
+*/
+  describe('Window Tests:', () => {
+    it('Opens a window',() => {
+     return app.client.waitUntilWindowLoaded()
+      .getWindowCount().should.eventually.equal(1);
+    })
+    it('Test the title',() => {
+      return app.client.waitUntilWindowLoaded()
+        .getTitle().should.eventually.equal('CodePad');
+    })
+    it('Test window visibility',() => {
+      app.browserWindow.isVisible().then((visible) => {
+        console.log('Is window Visible? : '+visible)
+      })
+    })
+  })
+  describe('Test window control',() => {
+    it('Tests min Button exists',() => {
+    return app.client.getAttribute('#min-button','title').then((btn) => {
+        assert.equal(btn,'Minimize')
+      })
+    })
+    it('Test max Button exists',() => {
+      return app.client.getAttribute('#max-button','title').then((btn) => {
+          assert.equal(btn,'Maximize')
+        })
+    })
+    it('Test close Button exists',() => {
+      return app.client.getAttribute('#close-button','title').then((btn) => {
+          assert.equal(btn,'Close')
+        })
+    })
+  })
+  describe('Test Menu',() => { 
+    it('Test JS menu',() => {
+     return app.client.getText('*=JS').then((e) => {
+       assert.equal(e,'+JS')
+     })
+    })
+    it('Test CSS menu',() => {
+     return app.client.getText('*=CSS').then((e) => {
+       assert.equal(e,'+CSS')
+     })
+    })
+    it('Test new Button',() => {
+      app.client.click('#new').then(() => {
+          return app.client.waitUntilWindowLoaded()
+           .getWindowCount().should.eventually.equal(2);
+     })
+    })
+    it('Test contribute link',() => {
+    return app.client.getAttribute('#contribute','href').then((link) => {
+      link.should.equal('https://www.github.com/thecodepad/codepad')
+    })
+  })
+    it('Test update link',() => {
+      return app.client.getAttribute('#check-updates','href').then((link) => {
+        link.should.equal('https://github.com/Jay9596/CodePad/releases')
+      })
+    })
+    it('Test about link',() => {
+      return app.client.getAttribute('#about','href').then((link) => {
+        link.should.equal('https://thecodepad.github.io/')
+      })
+    })
+    it('Test output window',() => {
+      app.client.getTagName('#output').then((output) => {
+        output.should.be.equal.to('iframe')
+      })
     })
   })
 })
