9532157bf994d2f6c4cf4b8d8b497da34a23a0ba
Abhishek Warokar <abhishek11@live.in>
2017-03-30 16:35:36 +0530

    lint fix

--- test/test.js
+++ test/test.js
@@ -4,9 +4,9 @@ const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 
 var app = new Application({
-      path: require('electron'),
-      args: [__dirname + '/../app/index.html']
-    })
+  path: require('electron'),
+  args: [__dirname + '/../app/index.html']
+})
 global.before(() => {
   chai.should()
   chai.use(chaiAsPromised)
@@ -23,86 +23,86 @@ describe('CodePad Testing:', function () {
       return app.stop()
     }
   })
-/*
-  it('Window Tests:', () => {
-    app.start().then(() => {
-      return app.browserWindow.isVisible()
-    }).then(isVisible => {
-      assert.equal(isVisible, true)
-    }).then(() => {
-      return app.client.getTitle()
-    }).then(title => {
-      assert.equal(title, 'CodePad')
-    }).catch(err => {
-      console.log('Test Failed:', err.message)
+  /*
+    it('Window Tests:', () => {
+      app.start().then(() => {
+        return app.browserWindow.isVisible()
+      }).then(isVisible => {
+        assert.equal(isVisible, true)
+      }).then(() => {
+        return app.client.getTitle()
+      }).then(title => {
+        assert.equal(title, 'CodePad')
+      }).catch(err => {
+        console.log('Test Failed:', err.message)
+      })
     })
-  })
-*/
+  */
   describe('Window Tests:', () => {
-    it('Opens a window',() => {
-     return app.client.waitUntilWindowLoaded()
-      .getWindowCount().should.eventually.equal(1);
-    })
-    it('Test the title',() => {
+    it('Opens a window', () => {
       return app.client.waitUntilWindowLoaded()
-        .getTitle().should.eventually.equal('CodePad');
+        .getWindowCount().should.eventually.equal(1)
     })
-    it('Test window visibility',() => {
+    it('Test the title', () => {
+      return app.client.waitUntilWindowLoaded()
+        .getTitle().should.eventually.equal('CodePad')
+    })
+    it('Test window visibility', () => {
       app.browserWindow.isVisible().then((visible) => {
-        console.log('Is window Visible? : '+visible)
+        console.log('Is window Visible? : ' + visible)
       })
     })
   })
-  describe('Test window control',() => {
-    it('Tests min Button exists',() => {
-    return app.client.getAttribute('#min-button','title').then((btn) => {
-        assert.equal(btn,'Minimize')
+  describe('Test window control', () => {
+    it('Tests min Button exists', () => {
+      return app.client.getAttribute('#min-button', 'title').then((btn) => {
+        assert.equal(btn, 'Minimize')
       })
     })
-    it('Test max Button exists',() => {
-      return app.client.getAttribute('#max-button','title').then((btn) => {
-          assert.equal(btn,'Maximize')
-        })
+    it('Test max Button exists', () => {
+      return app.client.getAttribute('#max-button', 'title').then((btn) => {
+        assert.equal(btn, 'Maximize')
+      })
     })
-    it('Test close Button exists',() => {
-      return app.client.getAttribute('#close-button','title').then((btn) => {
-          assert.equal(btn,'Close')
-        })
+    it('Test close Button exists', () => {
+      return app.client.getAttribute('#close-button', 'title').then((btn) => {
+        assert.equal(btn, 'Close')
+      })
     })
   })
-  describe('Test Menu',() => { 
-    it('Test JS menu',() => {
-     return app.client.getText('*=JS').then((e) => {
-       assert.equal(e,'+JS')
-     })
+  describe('Test Menu', () => {
+    it('Test JS menu', () => {
+      return app.client.getText('*=JS').then((e) => {
+        assert.equal(e, '+JS')
+      })
     })
-    it('Test CSS menu',() => {
-     return app.client.getText('*=CSS').then((e) => {
-       assert.equal(e,'+CSS')
-     })
+    it('Test CSS menu', () => {
+      return app.client.getText('*=CSS').then((e) => {
+        assert.equal(e, '+CSS')
+      })
     })
-    it('Test new Button',() => {
+    it('Test new Button', () => {
       app.client.click('#new').then(() => {
-          return app.client.waitUntilWindowLoaded()
-           .getWindowCount().should.eventually.equal(2);
-     })
+        return app.client.waitUntilWindowLoaded()
+          .getWindowCount().should.eventually.equal(2)
+      })
     })
-    it('Test contribute link',() => {
-    return app.client.getAttribute('#contribute','href').then((link) => {
-      link.should.equal('https://www.github.com/thecodepad/codepad')
+    it('Test contribute link', () => {
+      return app.client.getAttribute('#contribute', 'href').then((link) => {
+        link.should.equal('https://www.github.com/thecodepad/codepad')
+      })
     })
-  })
-    it('Test update link',() => {
-      return app.client.getAttribute('#check-updates','href').then((link) => {
+    it('Test update link', () => {
+      return app.client.getAttribute('#check-updates', 'href').then((link) => {
         link.should.equal('https://github.com/Jay9596/CodePad/releases')
       })
     })
-    it('Test about link',() => {
-      return app.client.getAttribute('#about','href').then((link) => {
+    it('Test about link', () => {
+      return app.client.getAttribute('#about', 'href').then((link) => {
         link.should.equal('https://thecodepad.github.io/')
       })
     })
-    it('Test output window',() => {
+    it('Test output window', () => {
       app.client.getTagName('#output').then((output) => {
         output.should.be.equal.to('iframe')
       })
