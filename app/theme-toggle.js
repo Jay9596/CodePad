3fac84fd407ec6864880af51a8e594782c645a69
Abhishek Warokar <abhishek11@live.in>
2017-04-15 14:23:53 +0530

    output fix + right border width ^

--- app/theme-toggle.js
+++ app/theme-toggle.js
@@ -11,21 +11,21 @@ function changeTheme() {
 
 	if (currentTheme === 'base16-ocean-dark') {
 		currentTheme = 'base16-tomorrow-light';
-		editors.style.borderBottom = '2px solid #EAEAEA';
 		for (let i = 0; i < editor.length; i++) {
 			editor[i].setOption('theme', 'base16-tomorrow-light');
-			editorBorder[i].style.borderRight = '2px solid #EAEAEA';
+			editorBorder[i].style.borderRight = '4px solid #E0E0E0';
 			editorLabels[i].style.color = '#646468';
-			editorLabels[i].style.backgroundColor = '#F2F2F2';
+			editorLabels[i].style.backgroundColor = '#D8D8D8';
 		}
+		editorsBottom.style.backgroundColor = "#E0E0E0";
 	} else {
 		currentTheme = 'base16-ocean-dark';
-		editors.style.borderBottom = '2px solid #121418';
 		for (let i = 0; i < editor.length; i++) {
 			editor[i].setOption('theme', 'base16-ocean-dark');
-			editorBorder[i].style.borderRight = '2px solid #121418';
+			editorBorder[i].style.borderRight = '4px solid #121418';
 			editorLabels[i].style.color = '#EAEAEA';
 			editorLabels[i].style.backgroundColor = '#232831';
 		}
+		editorsBottom.style.backgroundColor = "#121418";
 	}
 }
